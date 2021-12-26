f777f7ca2a9ca2d00df0f48df625d6892509caef
ChinoJunko <64834652+ChinoJunko@users.noreply.github.com>
2021-12-26 14:37:27 +0800

    init

---
+++ app/src/androidTest/java/dev/dworks/apps/anexplorer/AboutActivityTest.java
@@ -0,0 +1,21 @@
+package dev.dworks.apps.anexplorer;
+
+import junit.framework.*;
+
+public class AboutActivityTest extends TestCase {
+
+    public void testOnCreate() {
+    }
+
+    public void testGetTag() {
+    }
+
+    public void testStartActivity() {
+    }
+
+    public void testOnClick() {
+    }
+
+    public void testSetUpDefaultStatusBar() {
+    }
+}
